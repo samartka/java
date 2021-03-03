42bd89de8ff27c3597b4e3b1a3ecbde2e65c6391
samartka <samartka@yahoo.com>
2021-03-03 14:54:45 +0700

    Java section 1

--- src/com/tuc/helloWorld/controlFlow3.java
+++ src/com/tuc/helloWorld/controlFlow3.java
@@ -1,5 +1,5 @@
 package com.tuc.helloWorld;
-
+import java.util.Scanner; 
 public class controlFlow3 {
 
 	public static void main(String[] args) {
@@ -11,6 +11,8 @@ public class controlFlow3 {
 		sum1to10();
 		find12factor();
 		arrayLoop();
+		printHighToLow();
+		showOnlyEven();
 	}
 
 	public static void sum1to10() {
@@ -40,6 +42,29 @@ public class controlFlow3 {
 		for (int i : myArray) {
 			System.out.println(i);
 		}
+
+	}
+	
+	public static void printHighToLow() {
+		int i = 20;
+		do {
+			System.out.println(i);
+			i--;
+		}
+		while (i > 0);
+
+	}	
+	
+	public static void showOnlyEven() {
+		Scanner console = new Scanner(System.in);
+		int inputNum ;
+		do {
+			System.out.print("Enter number (will be exit if odd number was inputted !) ");
+		    inputNum = console.nextInt();
+		    if (inputNum%2==0) System.out.println(inputNum);
+		}
+		while(inputNum%2==0);
+		console.close();
 
 	}
 
